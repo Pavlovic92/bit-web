3e947b7a5aec0219cdc831cf0e9bd9b964b9a5df
unknown <stefan.pavlovic.serbia@gmail.com>
2020-03-03 23:43:43 +0100

    Uploading some home practice

---
+++ js-web/3.events/home-practice/js/javascript.js
@@ -0,0 +1,8 @@
+'use strict';
+/////////////
+
+document.querySelector('#second-list').className = "second-list-class";
+////////////
+
+var setBgColor = document.querySelector('ul li').className = "li-bg-color";
+
