b2d38b4c23e52232e9036a7cd503cf4ac3a47261
unknown <stefan.pavlovic.serbia@gmail.com>
2020-03-15 22:00:32 +0100

    Uploading new project in BitShow, API project

--- projects/BitShow-project/js/info.js
+++ projects/BitShow-project/js/info.js
@@ -1,45 +1,13 @@
-const a = window.location.search;
-let urlId = a.slice(a.length - 1);
-console.log(urlId);
+console.log(window);
 
+const showId = window.location.search.split('=')[1];
 
-function infoPage() {
-    const input = document.querySelector("#searchInput");
-    const getInputValue = input.value;
-    const urlShowSearch = "http://api.tvmaze.com/shows/" + urlId;
+const newXHR = new XMLHttpRequest();
+newXHR.open('GET', 'http://api.tvmaze.com/shows/' + showId);
 
-    const request = $.ajax({
-        url: urlShowSearch,
-        method: "GET"
-    });
-
-    request.done(function (data) {
-        console.log(data);
-        //////DIV WITH IMG///////
-        let infoDiv = $(".infoMain");
-        let imgDiv = $("<div>");
-        let imgInfo = $("<img>");
-        $(imgInfo).attr("src", data.image.original);
-        $(imgDiv).append(imgInfo);
-        $(infoDiv).append(imgDiv);
-
-        ////////DIV UL SEASONS////////
-        let ulDiv = $("<div>");
-        let ulFirst = $("<ul>");
-        let liFirst = $("<li>")
-
-        /////////DIV CAST//////////
-        let ulSecond = $("<ul>");
-        let liSecond = $("<li>")
-
-        let detailsDiv = $("<div>");
-        let h3Down = $("<h3>");
-        let pe = $("<p>");
-
-
-    });
-
+newXHR.onload = function () { //.onload gets called when the data is back and successful
+    console.log(JSON.parse(newXHR.responseText));
 
 }
-infoPage();
+newXHR.send();
 
